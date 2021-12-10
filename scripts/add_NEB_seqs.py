4fb21374d8383a47ed161556cc773832345bdf1f
Your Name <etholleman@ucdavis.edu>
2021-12-09 16:56:10 -0800

    bug fixes

--- scripts/add_NEB_seqs.py
+++ scripts/add_NEB_seqs.py
@@ -17,7 +17,7 @@ def check_for_5_prime_G(sequence):
 
 
 def add_NEB_seqs(T7_seq, scaffold, target):
-    return T7 + target + scaffold
+    return T7_seq + target + scaffold
 
 
 def format_target_seq_for_NEB_kit(T7_seq, scaffold, target_seq):
@@ -28,9 +28,12 @@ def format_target_seq_for_NEB_kit(T7_seq, scaffold, target_seq):
 def main():
     
     targets_df = pd.read_csv(str(snakemake.input), sep='\t')
+    config = snakemake.params['config']
+    print(config)
     
     T7_seq, scaffold = config['T7_PROMOTER'], config['THREE_PRIME_OVERLAP']
     targets_no_PAM = list(targets_df['target_no_pam'])
+    print(targets_no_PAM)
     NEB_oligos = [
         format_target_seq_for_NEB_kit(T7_seq, scaffold, target_seq)
         for target_seq in targets_no_PAM]

2a455d6e17ed8730e10dfb1c32a4eae72549967f
Your Name <etholleman@ucdavis.edu>
2021-12-11 12:34:01 -0800

    format with black

--- scripts/pamless.py
+++ scripts/pamless.py
@@ -3,47 +3,44 @@
 import pandas as pd
 from Bio.Seq import Seq
 
+
 def read_df(filepath):
-    
-    return pd.read_csv(str(filepath), sep='\t')
+
+    return pd.read_csv(str(filepath), sep="\t")
 
 
 def add_target_no_pam_column(df):
-    
+
     pamless_columns = []
     for index, row in df.iterrows():
-        
+
         # FlashFry outputs all sequences in the forward orientation even
         # if the target site is on the reverse strand. So the PAM site will
         # always be at the end of the sequence string.
         row = {
-            'target_no_pam': row['target'][:-3],
-            'start_no_pam': int(row['start']),
-            'end_no_pam': int(row['stop']) - 3
+            "target_no_pam": row["target"][:-3],
+            "start_no_pam": int(row["start"]),
+            "end_no_pam": int(row["stop"]) - 3,
         }
         pamless_columns.append(row)
-    
+
     # "cbind" the pamless columns to the old dataframe
     # https://stackoverflow.com/questions/33088010/pandas-column-bind-cbind-two-data-frames
-    df_c = pd.concat(
-        [df.reset_index(drop=True), pd.DataFrame(pamless_columns)], axis=1
-        )
+    df_c = pd.concat([df.reset_index(drop=True), pd.DataFrame(pamless_columns)], axis=1)
     return df_c
-        
 
 
 def main():
-    
+
     score_file = str(snakemake.input)
     df = read_df(score_file)
     df_no_pam = add_target_no_pam_column(df)
     print(df_no_pam)
-    assert 'target_no_pam' in df_no_pam.columns
-    
+    assert "target_no_pam" in df_no_pam.columns
+
     df_no_pam.dropna(inplace=True)
-    df_no_pam.to_csv(str(snakemake.output), sep='\t', index=False)
+    df_no_pam.to_csv(str(snakemake.output), sep="\t", index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    
--- scripts/select_targets.py
+++ scripts/select_targets.py
@@ -1,7 +1,5 @@
-
 import pandas as pd
 from Bio import SeqIO
-
 
 
 def get_ideal_points(min_dist, max_dist, number_targets):
@@ -12,58 +10,54 @@ def get_ideal_points(min_dist, max_dist, number_targets):
     return [i for i in range(min_dist, max_dist, step)]
 
 
-
 def filter_targets_by_strand(df, target_strand, nick_target=False):
-# nicking occurs on strand opposite target sequence
+    # nicking occurs on strand opposite target sequence
     print(target_strand)
     if nick_target:
-        return df.loc[df['orientation'] == target_strand]
+        return df.loc[df["orientation"] == target_strand]
     else:
-        if target_strand == 'FWD':
-            return df.loc[df['orientation'] == 'RVS']
-        elif target_strand == 'RVS':
-            return df.loc[df['orientation'] == 'FWD']
+        if target_strand == "FWD":
+            return df.loc[df["orientation"] == "RVS"]
+        elif target_strand == "RVS":
+            return df.loc[df["orientation"] == "FWD"]
         else:
-            raise TypeError('Target strand must be FWD or REV')
-    
+            raise TypeError("Target strand must be FWD or REV")
+
 
 def target_picker(df, ideal_point, filter_danger=True):
     # pick closest targets to each ideal point
 
     if filter_danger:
-        df = df.loc[(df['dangerous_GC'] == 'NONE') & (df['dangerous_polyT'] == 'NONE')]
-    
-    return df.iloc[(df['start']-ideal_point).abs().argsort()[:1]]
+        df = df.loc[(df["dangerous_GC"] == "NONE") & (df["dangerous_polyT"] == "NONE")]
+
+    return df.iloc[(df["start"] - ideal_point).abs().argsort()[:1]]
 
 
 def pick_targets(df, min_dist, max_dist, number_targets, target_strand, nicked_strand):
-    
+
     ideal_points = get_ideal_points(min_dist, max_dist, number_targets)
     df_stranded = filter_targets_by_strand(df, target_strand, nicked_strand)
     target_rows = []
     for each_point in ideal_points:
         target_rows.append(target_picker(df_stranded, each_point))
-    
+
     return pd.concat(target_rows)
 
 
 def main():
-    
-    config = snakemake.params['config']
-    all_targets = pd.read_csv(str(snakemake.input), sep='\t')
+
+    config = snakemake.params["config"]
+    all_targets = pd.read_csv(str(snakemake.input), sep="\t")
     targets = pick_targets(
-        all_targets, config['MIN_DIST'], config['MAX_DIST'], config['NUM_TARGETS'],
-        config['TARGET_STRAND'], config['NICKED_STRAND_IS_TARGET']
+        all_targets,
+        config["MIN_DIST"],
+        config["MAX_DIST"],
+        config["NUM_TARGETS"],
+        config["TARGET_STRAND"],
+        config["NICKED_STRAND_IS_TARGET"],
     )
-    targets.to_csv(str(snakemake.output), sep='\t', index=False)
+    targets.to_csv(str(snakemake.output), sep="\t", index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
-    
-
-    
-    
-        

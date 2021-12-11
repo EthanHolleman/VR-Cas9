900e806c2a794d0ce8387914469beb20132e9893
Your Name <etholleman@ucdavis.edu>
2021-12-11 12:34:16 -0800

    format with black

--- scripts/plot_targets.py
+++ scripts/plot_targets.py
@@ -7,7 +7,8 @@ from dna_features_viewer import (
 from colour import Color
 import yaml
 
-Color('blue')
+Color("blue")
+
 
 def read_relabel(filepath):
     with open(filepath) as handle:
@@ -29,14 +30,16 @@ def relabel(features, relabel_dict):
         for i, each_feat in enumerate(features):
             if feat_relab_dict["search"] in each_feat.label:  # search term in label
                 if "remove" in feat_relab_dict:  # if relabel dict has remove key
-                    delete_indexes.append(i)     # mark feature for destruction
+                    delete_indexes.append(i)  # mark feature for destruction
                 else:  # otherwise update feature attributes using values in
-                       # dictionary stored under attrs
+                    # dictionary stored under attrs
                     features[i].__dict__.update(feat_relab_dict["attrs"])
                     if features[i].label and features[i].label == "":
                         features[i].label = "test"
 
-    drawn_features = [feat for i, feat in enumerate(features) if i not in delete_indexes]
+    drawn_features = [
+        feat for i, feat in enumerate(features) if i not in delete_indexes
+    ]
     return drawn_features
 
 
@@ -50,7 +53,9 @@ def relabel_cas9_targets(features):
     targets = [i for i, feat in enumerate(features) if "Cas9" in feat.label]
     start_color = Color("#73FBD3")
     end_color = Color("#5C7AFF")
-    gradient = [c.hex for c in list(start_color.range_to(end_color, steps=len(targets)))]
+    gradient = [
+        c.hex for c in list(start_color.range_to(end_color, steps=len(targets)))
+    ]
     for i, each_index in enumerate(targets):
         features[each_index].label = f"Cas9 T{i}"
         features[each_index].color = gradient[i]
@@ -63,15 +68,15 @@ def draw_map(record_path, relabel_dict, output_path):
     feats = gr.features
     feats = relabel(feats, relabel_dict)
     feats = relabel_cas9_targets(feats)
-    #print(len(feats))
+    # print(len(feats))
     circ_gr = CircularGraphicRecord(sequence_length=len(gr.sequence), features=feats)
     ax, _ = circ_gr.plot()
-    ax.figure.savefig(output_path, bbox_inches='tight')
+    ax.figure.savefig(output_path, bbox_inches="tight")
 
 
 def main():
 
-    relabel_dict = read_relabel(snakemake.input["relabel_dict"])['relabel']
+    relabel_dict = read_relabel(snakemake.input["relabel_dict"])["relabel"]
     draw_map(str(snakemake.input["record"]), relabel_dict, str(snakemake.output))
 
 
